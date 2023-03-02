120b48ad312a596712c2766a30f4ad7ecf195543
asouzaaa <souza_amandaa@icloud.com>
2023-03-02 14:33:20 -0300

    quarto exercicio

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma string: ");
+            string str = Console.ReadLine();
+
+            string vogais = "aeiouAEIOU";
+            string consoantesEncontradas = " ";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!vogais.Contains(str[i].ToString()))
+                {
+                    consoantesEncontradas += str[i];
+                }
+            }
+
+            Console.WriteLine("Consoantes encontradas na string: " + consoantesEncontradas);
+        }
+    }
+}
